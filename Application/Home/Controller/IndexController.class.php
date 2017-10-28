26cd3c73446da1352b299adaff59745736b25fc5
Lich2013 <longzy2013@gmail.com>
2017-10-28 10:58:22 +0800

    debug

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -90,11 +90,12 @@ class IndexController extends BaseController {
         $currentData['today_learn_id'] = json_decode($currentData['today_learn_id']);
         sort($currentData['today_learn_id']);
         //检查学习题目上限
-        if ($currentData['today_group_count'] == 2 && !$this->isIdLearn($lesson_id, $currentData['today_learn_id'])) {
+        if ($currentData['today_group_count'] == 2 && !$this->isIdLearn($lesson_id, $currentData['today_learn_id']) && $lesson_id != 0) {
             $this->ajaxReturn(array(
-                'status' => 403,
-                'error'  => '每天最多只能学两组课程'
-            ));
+                    'status' => 403,
+                    'error' => '每天最多只能学两组课程',
+                )
+            );
         }
 
         //请求新题目时检查时间是否满足
